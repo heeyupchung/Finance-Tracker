33cc38051f6e8caf4e149dbd7686d9dd087d9793
heeyupchung <heeyupchung@gmail.com>
2022-02-22 14:13:38 -0800

    Initial commit

---
+++ src/component/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import 'NewExpense.css';
+
+const NewExpenses = () => {
+  return (
+    <div className="new-expenses">
+      <form>
+        
+      </form>
+    </div>
+  );
+}
+
+export default NewExpenses;
