42466c45fafc21e36086686cf01d9f5451ea17cf
heeyupchung <heeyupchung@gmail.com>
2022-03-03 15:55:57 -0800

    add expenses dynamically with state

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,31 +1,31 @@
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 import Card from '../UI/Card';
+import { useState } from 'react';
+
 
 const Expenses = ({items}) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
+  const filteredExpenses = items.filter(item => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className="expenses">
-      <ExpenseItem 
-        title={items[0].title}
-        date={items[0].date}
-        amount={items[0].amount}
-      />
-      <ExpenseItem 
-        title={items[1].title}
-        date={items[1].date}
-        amount={items[1].amount}
-      />
-      <ExpenseItem 
-        title={items[2].title}
-        date={items[2].date}
-        amount={items[2].amount}
-      />
-      <ExpenseItem 
-        title={items[3].title}
-        date={items[3].date}
-        amount={items[3].amount}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter 
+          selected={filteredYear} 
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </div>
   );
 }
 
